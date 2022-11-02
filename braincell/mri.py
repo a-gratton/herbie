daf359778034bbd85c352eac4c810578434b6900
Cindy Li <cindyli0213@hotmail.com>
2022-11-02 12:38:08 -0400

    Add Live Plotter Python Tool (#5)
    A command line tool that plots sensor data from serial in real time.
    
    You will need to have matplotlib installed to use this tool.
    
    IMU example:
    <img width="1440" alt="image"
    src="https://user-images.githubusercontent.com/36703188/199546132-c28a95f0-f0f8-411d-9228-0d347bd77dae.png">

---
+++ braincell/mri.py
@@ -0,0 +1,159 @@
+# !/usr/bin/env python3
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+import datetime as dt
+import serial
+import threading
+import sys, getopt
+
+DEFAULT_SERIAL_PORT = ''
+DEFAULT_VARIABLES_TO_PLOT = []
+DEFAULT_UNITS = ''
+DEFAULT_TITLE = 'MRI'
+DEFAULT_INTERVAL_MS = 10
+DEFAULT_MAX_SAMPLES = 200
+
+USAGE = '''Usage:
+python3 mri.py [OPTIONS]
+
+OPTIONS:
+   -h, --help
+      Outputs the help menu
+   -u, --units [units]
+      Sets the units of the plot
+   -t, --title [title]
+      Sets the title of the plot
+   -p, --port [port]
+      Sets the COM port to read serial data from
+   -i, --interval [interval_ms]
+      Sets the update frequency of the plot (default interval is {}ms)
+   -s, --samples [samples]
+      Sets the max number of samples to show on the plot at each instant (default is {} samples)
+   -v, --variable [variable]
+      Adds a variable to the plot
+
+NOTE: For the plotter to work, make sure you are continuously outputting lines of data over 
+the serial port, where each line has the following format:
+
+[VARIABLE1_NAME] [VARIABLE1_VALUE] [VARIABLE2_NAME] [VARIABLE2_VALUE] ...
+
+where VARIABLE#_NAME is a argument passed into the options list
+'''.format(DEFAULT_INTERVAL_MS, DEFAULT_MAX_SAMPLES)
+
+
+new_data_ready = False
+new_data_values = []
+run_read_data_thread = True
+
+
+def read_data(ser, variables_to_plot):
+    global run_read_data_thread
+    global new_data_ready
+    global new_data_values
+
+    for i in variables_to_plot:
+        new_data_values.append(0)
+
+    while run_read_data_thread:
+        line = ser.readline().strip().split()
+        try:
+            for i in range(len(line)-1):
+                for j in range(len(variables_to_plot)):
+                    if variables_to_plot[j] in line[i].decode("utf-8"):
+                        new_data_values[j] = float(line[i+1].decode("utf-8"))
+                        new_data_ready = True
+        except:
+            pass
+
+
+def animate(i, ax, start_time, variables_to_plot, title, units, max_samples, xs, ys):
+    global new_data_ready
+    global new_data_values
+
+    if new_data_ready:
+        xs.append(dt.datetime.now().timestamp() - start_time)
+        for i in range(len(variables_to_plot)):
+            ys[i].append(new_data_values[i])
+        new_data_ready = False
+
+    xs = xs[-max_samples:]
+    for i in range(len(ys)):
+        ys[i] = ys[i][-max_samples:]
+
+    ax.clear()
+
+    for i in range(len(variables_to_plot)):
+        ax.plot(xs, ys[i], label=variables_to_plot[i])
+        if len(ys[i]) > 0:
+            ax.annotate(str(ys[i][-1]), xy=(xs[-1], ys[i][-1]))
+
+    ax.legend()
+
+    plt.title(title)
+    plt.xlabel('Time (s)')
+    plt.ylabel(units)
+
+
+def main(argv):
+    units = DEFAULT_UNITS
+    title = DEFAULT_TITLE
+    variables_to_plot = DEFAULT_VARIABLES_TO_PLOT
+    serial_port = DEFAULT_SERIAL_PORT
+    interval_ms = DEFAULT_INTERVAL_MS
+    max_samples = DEFAULT_MAX_SAMPLES
+
+    # User args
+    try:
+        opts, _ = getopt.getopt(argv,"hu:t:p:i:s:v:", ['help', 'units=', 'title=', 'port=', 'interval=', 'samples=', 'variable='])
+    except getopt.GetoptError:
+        print(USAGE)
+        sys.exit(2)
+
+    for opt, arg in opts:
+        if opt in ('-h', '--help'):
+            print(USAGE)
+            sys.exit(0)
+        elif opt in ('-u', '--units'):
+            units = arg
+        elif opt in ('-t', '--title'):
+            title = arg
+        elif opt in ('-p', '--port'):
+            serial_port = arg
+        elif opt in ('-i', '--interval'):
+            interval_ms = int(arg)
+        elif opt in ('-s', '--samples'):
+            max_samples = int(arg)
+        elif opt in ('-v', '--variable'):
+            variables_to_plot.append(arg)
+
+    global run_read_data_thread
+
+    start_time = dt.datetime.now().timestamp()
+
+    ser = serial.Serial(
+        port=serial_port,
+        baudrate=115200,
+        parity=serial.PARITY_NONE,
+        stopbits=serial.STOPBITS_ONE,
+        bytesize=serial.EIGHTBITS,
+        timeout=1
+    )
+
+    read_data_thread = threading.Thread(target=read_data, args=(ser, variables_to_plot))
+    read_data_thread.start()
+
+    fig = plt.figure('MRI')
+    ax = fig.add_subplot(1, 1, 1)
+    xs = []
+    ys = []
+    for _ in variables_to_plot:
+        ys.append([])
+
+    ani = animation.FuncAnimation(fig, animate, fargs=(ax, start_time, variables_to_plot, title, units, max_samples, xs, ys), interval=interval_ms)
+    plt.show()
+
+    run_read_data_thread = False
+    read_data_thread.join()
+
+if __name__ == '__main__':
+    main(sys.argv[1:])
