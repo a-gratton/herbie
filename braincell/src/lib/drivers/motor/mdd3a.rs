b35268c737e656de5d4d4d4c3057485926bc4fe2
Kevin Gu <kg.kevin2001@gmail.com>
2022-11-05 13:04:34 -0400

    Little check to make sure pidout->power is within bounds

--- braincell/src/lib/drivers/motor/mdd3a.rs
+++ braincell/src/lib/drivers/motor/mdd3a.rs
@@ -15,7 +15,12 @@ pub fn convert_pidout_to_power(mut f: f32) -> (u16, u16) {
         f = -1.0 * f;
     }
 
-    let duty = (f * 24.0) as u16; //truncate
+    let mut duty = (f * 24.0) as u16; //truncate
+
+    //even though it's okay to set duty greater than max_duty, just in case something bad happens
+    if duty > 2400 {
+        duty = 2400;
+    }
 
     if dir == true {
         return (duty, 0);
@@ -36,15 +41,18 @@ where
     pub fn new(in1: (X, Y)) -> Self {
         Self { pwm: in1 }
     }
+
     pub fn set_power(&mut self, (speedx, speedy): (<X as PwmPin>::Duty, <Y as PwmPin>::Duty)) {
         self.pwm.0.set_duty(speedx);
         self.pwm.1.set_duty(speedy);
     }
+
     pub fn get_duty(&mut self) -> (<X as PwmPin>::Duty, <Y as PwmPin>::Duty) {
         let x = self.pwm.0.get_max_duty();
         let y = self.pwm.1.get_max_duty();
         return (x, y);
     }
+
     pub fn start(&mut self) {
         self.pwm.0.enable();
         self.pwm.1.enable();
