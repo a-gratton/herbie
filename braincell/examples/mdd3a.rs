1eb5bc901c26c311320e13b6247b9041dc8ad83a
Kevin Gu <kg.kevin2001@gmail.com>
2022-11-06 13:22:57 -0500

    Test pinout changes in example

--- braincell/examples/mdd3a.rs
+++ braincell/examples/mdd3a.rs
@@ -11,7 +11,7 @@ mod app {
     use panic_write::PanicHandler;
     use stm32f4xx_hal::{
         pac::USART2,
-        pac::{TIM1, TIM2, TIM3, TIM4},
+        pac::{TIM1, TIM2, TIM8, TIM12},
         prelude::*,
         serial::{Config, Serial, Tx},
         timer::pwm::PwmChannel,
@@ -24,10 +24,10 @@ mod app {
     #[local]
     struct Local {
         tx: core::pin::Pin<panic_write::PanicHandler<Tx<USART2>>>,
-        motor1: mdd3a::MDD3A<PwmChannel<TIM1, 0>, PwmChannel<TIM1, 1>>,
+        motor1: mdd3a::MDD3A<PwmChannel<TIM1, 2>, PwmChannel<TIM1, 3>>,
         motor2: mdd3a::MDD3A<PwmChannel<TIM2, 2>, PwmChannel<TIM2, 3>>,
-        motor3: mdd3a::MDD3A<PwmChannel<TIM3, 0>, PwmChannel<TIM3, 1>>,
-        motor4: mdd3a::MDD3A<PwmChannel<TIM4, 0>, PwmChannel<TIM4, 1>>,
+        motor3: mdd3a::MDD3A<PwmChannel<TIM8, 2>, PwmChannel<TIM8, 3>>,
+        motor4: mdd3a::MDD3A<PwmChannel<TIM12, 0>, PwmChannel<TIM12, 1>>,
     }
 
     #[monotonic(binds = SysTick, default = true)]
@@ -60,15 +60,15 @@ mod app {
         let mut tx = PanicHandler::new(serial);
 
         // set up PWM
-        let channels1 = (gpioa.pa8.into_alternate(), gpioa.pa9.into_alternate()); //D7 1/1 D8 1/2
+        let channels1 = (gpioa.pa10.into_alternate(), gpioa.pa11.into_alternate()); // 1/3  1/4
         let channels2 = (gpiob.pb10.into_alternate(), gpioa.pa3.into_alternate()); //D6 2/3 D0 2/4
-        let channels3 = (gpioa.pa6.into_alternate(), gpioc.pc7.into_alternate()); //d12 3/1 D9 3/2
-        let channels4 = (gpiob.pb6.into_alternate(), gpiob.pb7.into_alternate()); //D10 4/1 farleft on same line as lowgnd 4/2
+        let channels3 = (gpioc.pc8.into_alternate(), gpioc.pc9.into_alternate()); // 3/3  3/4
+        let channels4 = (gpiob.pb14.into_alternate(), gpiob.pb15.into_alternate()); // 4/3 4/4
 
         let pwm1 = ctx.device.TIM1.pwm_hz(channels1, 20.kHz(), &clocks).split();
         let pwm2 = ctx.device.TIM2.pwm_hz(channels2, 20.kHz(), &clocks).split();
-        let pwm3 = ctx.device.TIM3.pwm_hz(channels3, 20.kHz(), &clocks).split();
-        let pwm4 = ctx.device.TIM4.pwm_hz(channels4, 20.kHz(), &clocks).split();
+        let pwm3 = ctx.device.TIM8.pwm_hz(channels3, 20.kHz(), &clocks).split();
+        let pwm4 = ctx.device.TIM12.pwm_hz(channels4, 20.kHz(), &clocks).split();
 
         let mut motor1 = mdd3a::MDD3A::new(pwm1);
         let mut motor2 = mdd3a::MDD3A::new(pwm2);
