632f710e518813e9f41bb033d161045a6bd1eb79
Kevin Gu <kg.kevin2001@gmail.com>
2022-11-06 15:15:53 -0500

    cool stuff is happening

--- braincell/examples/mdd3a.rs
+++ braincell/examples/mdd3a.rs
@@ -25,9 +25,10 @@ mod app {
     struct Local {
         tx: core::pin::Pin<panic_write::PanicHandler<Tx<USART2>>>,
         motor1: mdd3a::MDD3A<PwmChannel<TIM1, 2>, PwmChannel<TIM1, 3>>,
-        motor2: mdd3a::MDD3A<PwmChannel<TIM2, 2>, PwmChannel<TIM2, 3>>,
+        motor2: mdd3a::MDD3A<PwmChannel<TIM2, 0>, PwmChannel<TIM2, 1>>,
         motor3: mdd3a::MDD3A<PwmChannel<TIM8, 2>, PwmChannel<TIM8, 3>>,
         motor4: mdd3a::MDD3A<PwmChannel<TIM12, 0>, PwmChannel<TIM12, 1>>,
+        motorsmth: mdd3a::MDD3A<PwmChannel<TIM2, 2>, PwmChannel<TIM2,3>>,
     }
 
     #[monotonic(binds = SysTick, default = true)]
@@ -60,27 +61,39 @@ mod app {
         let mut tx = PanicHandler::new(serial);
 
         // set up PWM
-        let channels1 = (gpioa.pa10.into_alternate(), gpioa.pa11.into_alternate()); // 1/3  1/4
-        let channels2 = (gpiob.pb10.into_alternate(), gpioa.pa3.into_alternate()); //D6 2/3 D0 2/4
-        let channels3 = (gpioc.pc8.into_alternate(), gpioc.pc9.into_alternate()); // 3/3  3/4
-        let channels4 = (gpiob.pb14.into_alternate(), gpiob.pb15.into_alternate()); // 4/3 4/4
 
-        let pwm1 = ctx.device.TIM1.pwm_hz(channels1, 20.kHz(), &clocks).split();
-        let pwm2 = ctx.device.TIM2.pwm_hz(channels2, 20.kHz(), &clocks).split();
-        let pwm3 = ctx.device.TIM8.pwm_hz(channels3, 20.kHz(), &clocks).split();
-        let pwm4 = ctx.device.TIM12.pwm_hz(channels4, 20.kHz(), &clocks).split();
+        //let channels2 = (gpioa.pa0.into_alternate(), gpioa.pa1.into_alternate(), gpiob.pb10.into_alternate(), gpioa.pa3.into_alternate()); //D6 2/3 D0 2/4
+        //let channelsx = (gpioa.pa0.into_alternate(), gpioa.pa1.into_alternate());
+        //let pwms = ctx.device.TIM2.pwm_hz(channels2, 20.kHz(), &clocks).split();
+        //let pmw2 = (pwms.0, pwms.1);
+        //let pwm4 = (pwms.2, pwms.3);
 
-        let mut motor1 = mdd3a::MDD3A::new(pwm1);
+        //let channels1 = (gpioa.pa10.into_alternate(), gpioa.pa11.into_alternate()); // 1/3  1/4
+        let channels2 = (gpioa.pa0.into_alternate(), gpioa.pa1.into_alternate(), gpiob.pb10.into_alternate(), gpioa.pa3.into_alternate()); //D6 2/3 D0 2/4
+        //let channels3 = (gpioc.pc8.into_alternate(), gpioc.pc9.into_alternate()); // 3/3  3/4
+        //let channels4 = (gpiob.pb14.into_alternate(), gpiob.pb15.into_alternate()); // 4/3 4/4
+
+        //let pwm1 = ctx.device.TIM1.pwm_hz(channels1, 20.kHz(), &clocks).split();
+        //let pwm2 = ctx.device.TIM2.pwm_hz(channels2, 20.kHz(), &clocks).split();
+        let pwms = ctx.device.TIM2.pwm_hz(channels2, 20.kHz(), &clocks).split();
+        let pwm2 = (pwms.0, pwms.1);
+        let pwm5 = (pwms.2, pwms.3);
+
+        //let pwm3 = ctx.device.TIM8.pwm_hz(channels3, 20.kHz(), &clocks).split();
+        //let pwm4 = ctx.device.TIM12.pwm_hz(channels4, 20.kHz(), &clocks).split();
+
+        //let mut motor1 = mdd3a::MDD3A::new(pwm1);
         let mut motor2 = mdd3a::MDD3A::new(pwm2);
-        let mut motor3 = mdd3a::MDD3A::new(pwm3);
-        let mut motor4 = mdd3a::MDD3A::new(pwm4);
+        let mut motorsmth = mdd3a::MDD3A::new(pwm5);
+        //let mut motor3 = mdd3a::MDD3A::new(pwm3);
+        //let mut motor4 = mdd3a::MDD3A::new(pwm4);
 
-        let max_duty = motor2.get_duty();
-        let md1 = max_duty.0;
-        let md2 = max_duty.1;
+        //let max_duty = motor2.get_duty();
+        //let md1 = max_duty.0;
+        //let md2 = max_duty.1;
 
-        writeln!(tx, "({md1},{md2})\r").unwrap();
-
+        //writeln!(tx, "({md1},{md2})\r").unwrap();
+/*
         let power = mdd3a::convert_pidout_to_power(0.0);
         motor1.set_power(power);
         motor2.set_power(power);
@@ -90,6 +103,7 @@ mod app {
         motor2.start();
         motor3.start();
         motor4.start();
+        */
 
         //writeln!(tx, "system initialized\r").unwrap();
         set_pwm_pwr::spawn_after(Duration::<u64, 1, 1000>::secs(1)).unwrap();
@@ -101,6 +115,7 @@ mod app {
                 motor2,
                 motor3,
                 motor4,
+                motorsmth,
             },
             init::Monotonics(mono),
         )
