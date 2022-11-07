714a6c15885f164bf23a4ddbbbe3c00f8cd530a4
Kevin Gu <kg.kevin2001@gmail.com>
2022-11-07 18:16:28 -0500

    Initial Encoder

--- braincell/examples/n20.rs
+++ braincell/examples/n20.rs
@@ -1,85 +1,112 @@
-#![deny(unsafe_code)]
 #![no_main]
 #![no_std]
 
-// Demonstrates the use of a rotary encoder. This example was tested
-// on a "black pill" USB C board:
-// https://stm32-base.org/boards/STM32F411CEU6-WeAct-Black-Pill-V2.0
-//
-// The rotary encoder A and B pins are connected to pins A0 and A1,
-// and they each have a 10K ohm pull-up resistor.
-
-use braincell::drivers::encoder;
-// Halt on panic
-use panic_halt as _;
-
-use cortex_m_rt::entry;
-use embedded_hal::Direction as RotaryDirection;
-use stm32f4xx_hal::{pac, prelude::*, qei::Qei};
-
-use stm32f4xx_hal::{
-    gpio::{Alternate, Output, Pin, PushPull, PB3, PB4, PB5},
-    pac::{SPI1, USART2},
-    prelude::*,
-    serial::{Config, Serial, Tx},
-    spi::{Mode, Phase, Polarity, Spi},
-};
-use core::fmt::Write;
-
-#[entry]
-fn main() -> ! {
-    let dp = pac::Peripherals::take().expect("Failed to get stm32 peripherals");
-    let cp = cortex_m::peripheral::Peripherals::take().expect("Failed to get cortex_m peripherals");
-
-    // Set up the system clock.
-    let rcc = dp.RCC.constrain();
-    let clocks = rcc.cfgr.freeze();
-
-    // Create a delay abstraction based on SysTick.
-    let mut delay = cp.SYST.delay(&clocks);
+#[rtic::app(device = stm32f4xx_hal::pac, peripherals = true, dispatchers = [SPI1])]
+mod app {
+    use braincell::drivers::encoder::n20;
+    
+    use core::fmt::Write;
+    use panic_write::PanicHandler;
+    use stm32f4xx_hal::{
+        pac::USART2,
+        pac::{TIM2, TIM3, TIM4, TIM5},
+        prelude::*,
+        serial::{Config, Serial, Tx},
+        qei::Qei,
+        
+    };
+    use systick_monotonic::{fugit::Duration, Systick};
+    use stm32f4xx_hal::gpio::Pin;
+    use stm32f4xx_hal::gpio::Alternate;
 
 
-    let gpioa = dp.GPIOA.split();
-    let gpiob = dp.GPIOB.split();
+    #[shared]
+    struct Shared {}
 
-    let tx_pin = gpioa.pa2.into_alternate();
-    let mut tx = Serial::tx(
-        dp.USART2,
-        tx_pin,
-        Config::default()
-                  .baudrate(115200.bps())
-                  .wordlength_8()
-                  .parity_none(),
-        &clocks,
+    #[local]
+    struct Local {
+        tx: core::pin::Pin<panic_write::PanicHandler<Tx<USART2>>>,
+        encoder1: n20::N20<Qei<TIM2, (Pin<'A', 15, Alternate<1>>, Pin<'B', 9, Alternate<1>>)>>,
+        encoder2: n20::N20<Qei<TIM3, (Pin<'A', 6, Alternate<2>>, Pin<'A', 7, Alternate<2>>)>>,
+        encoder3: n20::N20<Qei<TIM4, (Pin<'B', 6, Alternate<2>>, Pin<'B', 7, Alternate<2>>)>>,
+        encoder4: n20::N20<Qei<TIM5, (Pin<'A', 0, Alternate<2>>, Pin<'A', 1, Alternate<2>>)>>,
+
+    }
+
+    #[monotonic(binds = SysTick, default = true)]
+    type MonoTimer = Systick<1000>;
+
+    #[init]
+    fn init(ctx: init::Context) -> (Shared, Local, init::Monotonics) {
+        // configure clocks
+        let rcc = ctx.device.RCC.constrain();
+        let mono = Systick::new(ctx.core.SYST, 48_000_000);
+        let clocks = rcc.cfgr.sysclk(48.MHz()).freeze();
+
+        let gpioa = ctx.device.GPIOA.split();
+        let gpiob = ctx.device.GPIOB.split();
+
+        // set up uart tx
+        let tx_pin = gpioa.pa2.into_alternate();
+        let serial = Serial::tx(
+            ctx.device.USART2,
+            tx_pin,
+            Config::default()
+                .baudrate(115200.bps())
+                .wordlength_8()
+                .parity_none(),
+            &clocks,
         )
-      .unwrap();
+        .unwrap();
 
-    // Connect a rotary encoder to pins A0 and A1.
-    let encoder1_pins = (gpioa.pa15.into_alternate(), gpiob.pb9.into_alternate());
-    let encoder2_pins = (gpioa.pa6.into_alternate(), gpioa.pa7.into_alternate());
-    let encoder3_pins = (gpiob.pb6.into_alternate(), gpiob.pb7.into_alternate());
-    let encoder4_pins = (gpioa.pa0.into_alternate(), gpioa.pa1.into_alternate());
+        let mut tx = PanicHandler::new(serial);
 
-    let encoder1_timer = dp.TIM2;
-    let encoder2_timer = dp.TIM3;
-    let encoder3_timer = dp.TIM4;
-    let encoder4_timer = dp.TIM5;
+        let encoder1_pins = (gpioa.pa15.into_alternate(), gpiob.pb9.into_alternate());
+        let encoder2_pins = (gpioa.pa6.into_alternate(), gpioa.pa7.into_alternate());
+        let encoder3_pins = (gpiob.pb6.into_alternate(), gpiob.pb7.into_alternate());
+        let encoder4_pins = (gpioa.pa0.into_alternate(), gpioa.pa1.into_alternate());
+    
+        let encoder1_timer = ctx.device.TIM2;
+        let encoder2_timer = ctx.device.TIM3;
+        let encoder3_timer = ctx.device.TIM4;
+        let encoder4_timer = ctx.device.TIM5;
+    
+        let encoder1_qei = Qei::new(encoder1_timer, encoder1_pins);
+        let encoder2_qei = Qei::new(encoder2_timer, encoder2_pins);
+        let encoder3_qei = Qei::new(encoder3_timer, encoder3_pins);
+        let encoder4_qei = Qei::new(encoder4_timer, encoder4_pins);
+        let mut encoder1 = n20::N20::new(encoder1_qei);
+        let mut encoder2 = n20::N20::new(encoder2_qei);
+        let mut encoder3 = n20::N20::new(encoder3_qei);
+        let mut encoder4 = n20::N20::new(encoder4_qei);
 
-    let encoder1_qei = Qei::new(encoder1_timer, encoder1_pins);
-    let encoder2_qei = Qei::new(encoder2_timer, encoder2_pins);
-    let encoder3_qei = Qei::new(encoder3_timer, encoder3_pins);
-    let encoder4_qei = Qei::new(encoder4_timer, encoder4_pins);
-    let mut encoder1 = encoder::n20::QeiWrapper::new(encoder1_qei);
-    let mut encoder2 = encoder::n20::QeiWrapper::new(encoder2_qei);
-    let mut encoder3 = encoder::n20::QeiWrapper::new(encoder3_qei);
-    let mut encoder4 = encoder::n20::QeiWrapper::new(encoder4_qei);
+        writeln!(tx, "system initialized\r").unwrap();
 
-    loop {
-        //let new_count = rotary_encoder.count();
-        let new_count = encoder1.get_speed();
-        //writeln!(tx, "Count: {new_count}\r").unwrap();
-        writeln!(tx, "Count: {new_count}\r").unwrap();
+        read_speed::spawn_after(Duration::<u64, 1, 1000>::secs(1)).unwrap();
+        (
+            Shared {},
+            Local {
+                tx,
+                encoder1,
+                encoder2,
+                encoder3,
+                encoder4,
+            },
+            init::Monotonics(mono),
+        )
+    }
 
-        delay.delay_ms(10_u32);
+    #[task(local=[tx, encoder1, encoder2, encoder3, encoder4], shared=[])]
+    fn read_speed(cx: read_speed::Context) {
+
+        let timeis: u64 = monotonics::now().ticks();
+        let new_count1 = cx.local.encoder1.get_speed(timeis);
+        let new_count2 = cx.local.encoder2.get_speed(timeis);
+        let new_count3 = cx.local.encoder3.get_speed(timeis);
+        let new_count4 = cx.local.encoder4.get_speed(timeis);
+        writeln!(cx.local.tx, "deg/s: {new_count4}\r").unwrap();
+
+        // run at 100 Hz
+        read_speed::spawn_after(Duration::<u64, 1, 1000>::millis(10)).unwrap();
     }
 }
--- braincell/src/lib/drivers/encoder/n20.rs
+++ braincell/src/lib/drivers/encoder/n20.rs
@@ -5,27 +5,52 @@ use embedded_hal::Direction as RotaryDirection;
 use stm32f4xx_hal::prelude::_embedded_hal_Qei;
 //use stm32f4xx_hal::{pac, prelude::*, qei::Qei};
 
-pub struct QeiWrapper <X> {
-    encoder: X
+pub struct N20 <X> {
+    encoder: X,
+    pasttick: u64,
+    pastcount: u32,
 }
 
-impl<X> QeiWrapper<X> 
+impl<X> N20<X> 
 where
-X: _embedded_hal_Qei
+X: _embedded_hal_Qei, u32: From<<X as _embedded_hal_Qei>::Count>
 {
     pub fn new(qei: X) -> Self {
         Self {
             encoder: qei,
+            pasttick: 0,
+            pastcount: 0,
         }
     }
-     pub fn get_speed(&mut self) -> <X as _embedded_hal_Qei>::Count {
-        let mut fw = true;
+     pub fn get_speed(&mut self, currtick: u64) -> f32 {
+        let timediff = ((currtick - self.pasttick) as f32) * 0.001;
+        self.pasttick = currtick;
+
+
         let count = self.encoder.count();
-        match self.encoder.direction() {
-            RotaryDirection::Upcounting => fw = true,
-            RotaryDirection::Downcounting => fw = false,
+        let mut currcount = <<X as _embedded_hal_Qei>::Count as Into<u32>>::into(count); //need to take into account u16 overflow
+
+        let mut countdiff:u32 = 0;
+        /*
+        if currcount - self.pastcount > 32000 {
+            if currcount > self.pastcount {
+                let smth = self.pastcount + 65536;
+                countdiff = smth - currcount;
+            } else {
+                let smth = currcount + 65536;
+                countdiff = smth - self.pastcount;
+            }
         }
-        return count;
+        self.pastcount = currcount;
+        */
+        countdiff = currcount - self.pastcount;
+        self.pastcount = currcount;
+
+        let countselapsed = (countdiff as f32);
+        let degreeselapsed = countselapsed / 1400.0 * 360.0;
+        let degreespersecond = degreeselapsed/timediff;
+
+        return degreespersecond;
 
      }
 }
