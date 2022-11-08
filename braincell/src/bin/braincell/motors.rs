2628b82ff521a315d00a454f5e3505497e57d9a3
Aidan Gratton <gratton.aidan@gmail.com>
2022-11-08 15:52:47 -0500

    Make Encoders Local to Speed Control Thread
    Adds the encoder objects as locals to the speed control thread. Also
    samples the motor velocities in the thread instead of relying on
    external shared variable updates.

--- braincell/src/bin/braincell/motors.rs
+++ braincell/src/bin/braincell/motors.rs
@@ -1,4 +1,6 @@
-use crate::app::speed_control;
+use crate::app::{monotonics, speed_control};
+use crate::config::sys_config;
+use braincell::controller::motor::VelocityMeasurement;
 use rtic::Mutex;
 use systick_monotonic::fugit::Duration;
 
@@ -6,9 +8,14 @@ pub fn speed_control(mut cx: speed_control::Context) {
     cx.shared
         .motor_setpoints
         .lock(|motor_setpoints| cx.local.motors.set_speed_targets(motor_setpoints));
-    cx.shared.motor_velocities.lock(|motor_velocities| {
-        cx.local.motors.step(motor_velocities);
-    });
+    let curtime = monotonics::now().ticks() as f32 * sys_config::SECONDS_PER_TICK;
+    let vels = VelocityMeasurement {
+        f_left: cx.local.encoder_f_left.get_speed(curtime),
+        r_left: cx.local.encoder_r_left.get_speed(curtime),
+        f_right: cx.local.encoder_f_right.get_speed(curtime),
+        r_right: cx.local.encoder_r_right.get_speed(curtime),
+    };
+    cx.local.motors.step(&vels);
     // run at 1 kHz
     speed_control::spawn_after(Duration::<u64, 1, 1000>::millis(1)).unwrap();
 }
